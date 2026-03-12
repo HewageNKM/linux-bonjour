9c81bf5a5fccaef193455ce51f581907f9355fcc
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2026-03-12 23:53:12 +0530

    chore: bump version to v2.2.9

--- scripts/build_deb.sh
+++ scripts/build_deb.sh
@@ -6,7 +6,7 @@ set -e
 
 # v1.1.10: Make script robust to execution directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-VERSION="2.2.8"
+VERSION="2.2.9"
 PROJECT_ROOT="$( dirname "$SCRIPT_DIR" )"
 cd "$PROJECT_ROOT"
 
